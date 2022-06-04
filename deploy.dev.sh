98372c84b05c87decbbf6690dfaf5f2ec30d8aff
Johan Book <13253042+johanbook@users.noreply.github.com>
2022-06-04 21:09:42 +0200

    feat: use analytics services

--- deploy.dev.sh
+++ deploy.dev.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 scripts/deploy \
+	-f manifests/analytics.yaml \
 	-f manifests/cloud.yaml \
 	-f manifests/gateway.dev.yaml \
 	-f manifests/monitoring.yaml
--- deploy.prod.sh
+++ deploy.prod.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 scripts/deploy \
+	-f manifests/analytics.yaml \
 	-f manifests/cloud.yaml \
 	-f manifests/monitoring.yaml \
 	-f manifests/gateway.prod.yaml
